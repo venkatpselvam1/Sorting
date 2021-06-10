8e403d2283c453fb58a7f3e127c174a127af6aa7
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-06-10 20:58:15 +0530

    Quick sort

--- SortingAlogrithems/QuickSort/Program.cs
+++ SortingAlogrithems/QuickSort/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace QuickSort
 {
@@ -10,6 +11,9 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
+            var arr = new int[] { 10, 1, 3, 7, 2, 5, 21, 17, 11 };
+            QuickSort.Sort(arr);
+            Utilities.Utilities.PrintArray(arr);
         }
     }
 }
--- SortingAlogrithems/QuickSort/QuickSort.cs
+++ SortingAlogrithems/QuickSort/QuickSort.cs
@@ -10,7 +10,33 @@ namespace QuickSort
     {
         public static void Sort(int[] arr)
         {
-
+            int start = 0;
+            int end = arr.Length - 1;
+            QuickSortArr(arr, start, end);
+        }
+        public static void QuickSortArr(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                int pi = GetPartiton(arr, start, end);
+                QuickSortArr(arr, start, pi - 1);
+                QuickSortArr(arr, pi + 1, end);
+            }
+        }
+        public static int GetPartiton(int[] arr, int start, int end)
+        {
+            var pivotValue = arr[end];
+            int j = start - 1;
+            for (int i = start; i < end; i++)
+            {
+                if (arr[i] > pivotValue)
+                {
+                    j++;
+                    Utilities.Utilities.Swap(arr, i, j);
+                }
+            }
+            Utilities.Utilities.Swap(arr, j+1, end);
+            return j + 1;
         }
     }
 }
