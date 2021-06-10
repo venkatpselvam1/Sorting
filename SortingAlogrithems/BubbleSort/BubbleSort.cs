f8d4d0e5e0615690929150ed1a47d6353ffe24cf
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-06-10 12:45:16 +0530

    buble sort

---
+++ SortingAlogrithems/BubbleSort/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        internal static void Sort(int[] arr)
+        {
+            var length = arr.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length-i-1; j++)
+                {
+                    if (arr[j] >arr[j+1])
+                    {
+                        Swap(arr, i, j);
+                    }
+                }
+            }
+        }
+        public static void Swap(int[] arr, int a, int b)
+        {
+            var temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+    }
+}
---
+++ SortingAlogrithems/BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 10, 1, 3, 7, 2, 5, 21, 17, 11 };
+            BubbleSort.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
