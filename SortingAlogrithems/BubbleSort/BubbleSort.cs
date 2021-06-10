1c4a3e3d8f3ea97b138ad458a699bd2ee696c03a
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-06-10 17:38:06 +0530

    selection sort

--- SortingAlogrithems/BubbleSort/BubbleSort.cs
+++ SortingAlogrithems/BubbleSort/BubbleSort.cs
@@ -11,13 +11,13 @@ namespace BubbleSort
         internal static void Sort(int[] arr)
         {
             var length = arr.Length;
-            for (int i = 0; i < length - 1; i++)
+            for (int i = 0; i < length-1; i++)
             {
                 for (int j = 0; j < length-i-1; j++)
                 {
                     if (arr[j] >arr[j+1])
                     {
-                        Utilities.Utilities.Swap(arr, i, j);
+                        Utilities.Utilities.Swap(arr, j, j+1);
                     }
                 }
             }
---
+++ SortingAlogrithems/SelectionSort/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        public static void Sort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                bool canSwap = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (!canSwap && arr[j] > arr[i])
+                    {
+                        canSwap = true;
+                    }
+                    if (canSwap)
+                    {
+                        Utilities.Utilities.Swap(arr, j, i);
+                    }
+                }
+            }
+        }
+    }
+}
