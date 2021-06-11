429c7b5387202963bb8bcc2e0b117f00f4a77e92
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-06-11 09:07:15 +0530

    Shell sort

---
+++ SortingAlogrithems/ShellSort/ShellSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+    class ShellSort
+    {
+        public static void Sort(int[] arr)
+        {
+            var length = arr.Length;
+            var halfLength = length / 2;
+            for (int k = halfLength; k > 0; k=k/2)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    var smallestInd = i;
+                    for (int j = i + k; j < length; j=j+k)
+                    {
+                        if (arr[smallestInd] > arr[j])
+                        {
+                            smallestInd = j;
+                        }
+                    }
+                    Utilities.Utilities.Swap(arr, i, smallestInd);
+                }
+            }
+        }
+    }
+}
